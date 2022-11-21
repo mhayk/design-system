389e96ab41526520f6df2624bed376aadbe4117f
Mhayk Whandson <hi@mhayk.com>
2022-11-21 11:15:26 +0000

    feat: create and document Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@mk-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/mhayk.png',
+    alt: 'Mhayk Whandson',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const withFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
