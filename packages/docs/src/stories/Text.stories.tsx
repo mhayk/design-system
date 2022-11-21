43ba0e303df647a408545fd4ace55b359b4a8b97
Mhayk Whandson <hi@mhayk.com>
2022-11-21 10:24:54 +0000

    feat: create and document the component Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@mk-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatem perferendis sit. Cum, consequuntur quia quos corrupti libero hic, voluptates recusandae repudiandae sed voluptate accusamus debitis quas natus perspiciatis blanditiis!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTrag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
