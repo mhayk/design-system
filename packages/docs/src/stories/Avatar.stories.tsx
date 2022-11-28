9eb12d719190f88d2122efd1b42af23c62af002a
Mhayk Whandson <hi@mhayk.com>
2022-11-28 14:15:25 +0000

    feat: add argTypes in the component's documentations

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/mhayk.png',
     alt: 'Mhayk Whandson',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -6,8 +6,29 @@ export default {
   component: Text,
 
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatem perferendis sit. Cum, consequuntur quia quos corrupti libero hic, voluptates recusandae repudiandae sed voluptate accusamus debitis quas natus perspiciatis blanditiis!',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: 'inline-radio',
+    },
   },
 } as Meta<TextProps>
 
